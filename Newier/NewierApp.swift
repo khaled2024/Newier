116a2c17da468392dbd908522dccb91030a2780a
khaled hussien <57367756+khaled2024@users.noreply.github.com>
2023-01-31 17:25:05 +0200

    Initial Commit

---
+++ Newier/NewierApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewierApp.swift
+//  Newier
+//
+//  Created by KhaleD HuSsien on 31/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct NewierApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
